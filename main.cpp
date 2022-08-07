3181a9490b1a3bb78e94e1aeba1e68444b0d5465
kkm213 <kkm213@korea.ac.kr>
2022-08-07 21:13:41 +0900

    upload

--- main.cpp
+++ main.cpp
@@ -46,8 +46,8 @@ int main(int argc, char *argv[])
 	for (int i = 0; i < 256; i++)
 	{
 		victim[3] = i;
-		request(dev, pcap, broad_mac, attacker_mac, attacker_mac, attacker_ip, empty_mac, victim_ip, 0);
-		reply(dev, pcap, victim_mac, victim_ip);
+		request(dev, pcap, broad_mac, attacker_mac, attacker_mac, attacker_ip, empty_mac, victim, 0);
+		reply(dev, pcap, victim_mac, victim);
 		printf("sender mac addr : ");
 		print_mac(victim_mac);
 	}
