dcb6c0a435c4eb9f9a617dca2fc3be89538eba3a
kkm213 <kkm213@korea.ac.kr>
2022-08-07 19:03:44 +0900

    upload

--- send_arp.h
+++ send_arp.h
@@ -37,26 +37,48 @@ int request(const char *dev, pcap_t *pcap, u_int8_t *dest_mac, u_int8_t *source_
     return 0;
 }
 
-int reply(const char *dev, pcap_t *pcap, u_int8_t *mac)
+// int reply(const char *dev, pcap_t *pcap, u_int8_t *mac)
+// {
+//     struct pcap_pkthdr *header;
+//     const u_char *packet;
+//     int res = pcap_next_ex(pcap, &header, &packet);
+//     if (res != 1)
+//     {
+//         printf("error!\n");
+//         return -1;
+//     }
+//     EthArpPacket *arppkt;
+//     arppkt = (EthArpPacket *)packet;
+//     if (arppkt->eth_.type_ == htons(EthHdr::Arp))
+//     {
+//         if (arppkt->arp_.pro_ == htons(EthHdr::Ip4))
+//         {
+//             copy_mac(arppkt->arp_.smac_, mac);
+//         }
+//     }
+//     return 0;
+// }
+
+int reply(const char *dev, pcap_t *pcap, u_int8_t *mac, u_int8_t *ip)
 {
     struct pcap_pkthdr *header;
     const u_char *packet;
-    int res = pcap_next_ex(pcap, &header, &packet);
-    if (res != 1)
+    while (1)
     {
-        printf("error!\n");
-        return -1;
-    }
-    EthArpPacket *arppkt;
-    arppkt = (EthArpPacket *)packet;
-    if (arppkt->eth_.type_ == htons(EthHdr::Arp))
-    {
-        if (arppkt->arp_.pro_ == htons(EthHdr::Ip4))
+        int res = pcap_next_ex(pcap, &header, &packet);
+        if (res != 1)
         {
-            print_mac(arppkt->arp_.smac_);
+            printf("error!\n");
+            return -1;
+        }
+        EthArpPacket *arppkt;
+        arppkt = (EthArpPacket *)packet;
+        if (arppkt->eth_.type_ == htons(EthHdr::Arp) && arppkt->arp_.pro_ == htons(EthHdr::Ip4) && if_same_ip(arppkt->arp_.sip, ip))
+        {
+            copy_mac(arppkt->arp_.smac_, mac);
+            break;
         }
     }
-    copy_mac(arppkt->arp_.smac_, mac);
     return 0;
 }
 
