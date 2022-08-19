ee50cf7c7455ffba5b31ac3624304d003cb5cf0c
kkm213 <kkm213@korea.ac.kr>
2022-08-19 17:49:00 +0900

    upload

--- send_arp.h
+++ send_arp.h
@@ -183,8 +183,8 @@ int relay(const char *dev, pcap_t *pcap, u_int8_t *attacker_mac, u_int8_t *sende
                 ip_pkt->ip_.ip_offset = 0;
                 //  memcpy(pkt->eth_.dmac_, sender_mac, 6);
                 //  memcpy(pkt->eth_.smac_, attacker_mac, 6);
-                // int res = pcap_sendpacket(pcap, (u_char *)pkt, header->len);
-                int res = pcap_sendpacket(pcap, (u_char *)pkt, sendsize);
+                int res = pcap_sendpacket(pcap, (u_char *)pkt, header->len);
+                // int res = pcap_sendpacket(pcap, (u_char *)pkt, sendsize);
                 if (res != 0)
                 {
                     fprintf(stderr, "pcap_sendpacket return %d error=%s\n", res, pcap_geterr(pcap));
