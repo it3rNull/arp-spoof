b0a0f21995e84a54a572648c83286face3c9b822
kkm213 <kkm213@korea.ac.kr>
2022-08-19 19:21:12 +0900

    upload

--- send_arp.h
+++ send_arp.h
@@ -192,7 +192,7 @@ int relay(const char *dev, pcap_t *pcap, u_int8_t *attacker_mac, u_int8_t *sende
                 //     i++;
                 // }
                 sendsize = header->len - 400 * i;
-                // ip_pkt->ip_.ip_len = htons(sendsize - 14);
+                ip_pkt->ip_.ip_len = htons(sendsize - 14);
                 // ip_pkt->ip_.ip_offset = htons(0b0000000000000000);
                 // memcpy(pkt + 34, data + 400 * i, sendsize);
                 printf("sendsize : %d\n", sendsize);
