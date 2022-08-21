f9b1d561fd0ff8ee9d697b31a9f95fc88c731d27
kkm213 <kkm213@korea.ac.kr>
2022-08-21 21:37:20 +0900

    upload

--- checksum.cpp
+++ checksum.cpp
@@ -4,14 +4,14 @@ uint16_t calc_checksum_ip(IpHdr *ip_)
 {
     uint32_t *sum = 0;
     uint16_t *block = (uint16_t *)ip_;
-    uint16_t *carry = (uint16_t *)sum;
-
+    uint16_t *carry;
     for (int i = 0; i < 10; i++)
     {
         *sum += block[i];
     }
 
     printf("sum : %x\n", *sum);
+    carry = (uint16_t *)sum;
 
     printf("carry : %x\n", *carry);
 }
