17c77c3c9e4b19542a512f8be40f6db06b90a87c
kkm213 <kkm213@korea.ac.kr>
2022-08-22 16:58:06 +0900

    upload

--- send_arp.h
+++ send_arp.h
@@ -77,11 +77,11 @@ int reply(const char *dev, pcap_t *pcap, u_int8_t *mac, u_int8_t *ip)
         EthArpPacket *arppkt;
         arppkt = (EthArpPacket *)packet;
 
-        // if (arppkt->eth_.type_ == htons(EthHdr::Arp) && arppkt->arp_.pro_ == htons(EthHdr::Ip4) && (memcmp(arppkt->arp_.sip, ip, 4) == 0))
-        if (arppkt->eth_.type_ == htons(EthHdr::Arp) && arppkt->arp_.pro_ == htons(EthHdr::Ip4) && if_same_ip(arppkt->arp_.sip, ip))
+        // if (arppkt->eth_.type_ == htons(EthHdr::Arp) && arppkt->arp_.pro_ == htons(EthHdr::Ip4) && if_same_ip(arppkt->arp_.sip, ip))
+        if (arppkt->eth_.type_ == htons(EthHdr::Arp) && arppkt->arp_.pro_ == htons(EthHdr::Ip4) && (!memcmp(arppkt->arp_.sip, ip, 4)))
         {
-            copy_mac(arppkt->arp_.smac_, mac);
-            // memcpy(mac, arppkt->arp_.smac_, 6);
+            // copy_mac(arppkt->arp_.smac_, mac);
+            memcpy(mac, arppkt->arp_.smac_, 6);
             break;
         }
     }
