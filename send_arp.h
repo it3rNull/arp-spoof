f4902aec899a2a1769951dfe7cc1de8f09094695
kkm213 <kkm213@korea.ac.kr>
2022-08-22 16:21:50 +0900

    upload

--- send_arp.h
+++ send_arp.h
@@ -91,7 +91,7 @@ int reply(const char *dev, pcap_t *pcap, u_int8_t *mac, u_int8_t *ip)
 // int relay(const char *dev, pcap_t *pcap, u_int8_t *attacker_mac, u_int8_t *sender_mac, u_int8_t *target_mac, u_int8_t *sender_ip, u_int8_t *target_ip)
 int relay(const char *dev, pcap_t *pcap, u_int8_t *attacker_mac, list *targets, int count)
 {
-    const int fragment_size = 1440;
+    const int fragment_size = 1400;
     while (true)
     {
         struct pcap_pkthdr *header;
