9e854169ca9c04a893491f288e5d68585108393c
kkm213 <kkm213@korea.ac.kr>
2022-08-22 21:21:30 +0900

    upload

--- send_arp.h
+++ send_arp.h
@@ -93,15 +93,6 @@ int relay(const char *dev, pcap_t *pcap, u_int8_t *attacker_mac, list *targets, 
 
         for (int i = 0; i < count; i++)
         {
-            if (kbhit())
-            {
-                request(dev, pcap, targets[i].target_mac, attacker_mac, targets[i].sender_mac, targets[i].sender_ip, targets[i].target_mac, targets[i].target_ip, htons(ArpHdr::Reply));
-                request(dev, pcap, targets[i].sender_mac, attacker_mac, targets[i].target_mac, targets[i].target_ip, targets[i].sender_mac, targets[i].sender_ip, htons(ArpHdr::Reply));
-                printf("attack stopped ! byebye\n");
-                pcap_close(pcap);
-                free(targets);
-                exit(0);
-            }
             if ((pkt->eth_.type_ == htons(EthHdr::Arp)) && (pkt->arp_.pro_ == htons(EthHdr::Ip4)) && (!memcmp(pkt->arp_.smac_, targets[i].target_mac, 6)) && (!memcmp(pkt->arp_.tip, targets[i].sender_ip, 4)))
             {
                 printf("where is sender?\n");
