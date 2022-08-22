539e70c1ce365a49250af1926639dcc90a16cc39
kkm213 <kkm213@korea.ac.kr>
2022-08-22 21:14:31 +0900

    upload

--- send_arp.h
+++ send_arp.h
@@ -93,7 +93,7 @@ int relay(const char *dev, pcap_t *pcap, u_int8_t *attacker_mac, list *targets, 
 
         for (int i = 0; i < count; i++)
         {
-            if (_kbhit())
+            if (kbhit())
             {
                 request(dev, pcap, targets[i].target_mac, attacker_mac, targets[i].sender_mac, targets[i].sender_ip, targets[i].target_mac, targets[i].target_ip, htons(ArpHdr::Reply));
                 request(dev, pcap, targets[i].sender_mac, attacker_mac, targets[i].target_mac, targets[i].target_ip, targets[i].sender_mac, targets[i].sender_ip, htons(ArpHdr::Reply));
