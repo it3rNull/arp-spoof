a2192ede4d9085c3cc084a9996de3a1605daef46
kkm213 <kkm213@korea.ac.kr>
2022-08-22 21:10:08 +0900

    upload

--- main.cpp
+++ main.cpp
@@ -70,6 +70,7 @@ int main(int argc, char *argv[])
 				request(dev, pcap, targets[i].sender_mac, attacker_mac, attacker_mac, targets[i].target_ip, targets[i].sender_mac, targets[i].sender_ip, htons(ArpHdr::Reply));
 				request(dev, pcap, targets[i].target_mac, attacker_mac, attacker_mac, targets[i].sender_ip, targets[i].target_mac, targets[i].target_ip, htons(ArpHdr::Reply));
 			}
+			printf("press any key to stop .. \n");
 			relay(dev, pcap, attacker_mac, targets, count);
 		}
 		else if (todo_choice == 2)
